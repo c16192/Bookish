c50f6e99f75c41361c6ebb513b5c9ff49446d9f2
Marton Erdos <me412@cam.ac.uk>
2018-06-28 15:47:51 +0100

    Added DB API methods. TODO: testing!

--- api/DBConnection.ts
+++ api/DBConnection.ts
@@ -1,26 +1,101 @@
+import {Book, User, BookCopy, RawBookCopy, getBookCopy, getRawBookCopy} from "./DataTypes";
+
 const pgp = require('pg-promise')(/*options*/);
 
 export class DBConnection {
     private db = pgp(process.env.POSTGRES_URI);
 
-    public getAllCopies() {
-        return this.db.any('SELECT * FROM copies');
+    public addBook(book: Book): Promise<void> {
+        return this.db.none(`INSERT INTO books VALUES ${JSON.stringify(book)}`);
+    }
+
+    public addCopy(copy: BookCopy): Promise<void> {
+        return this.db.none(`INSERT INTO copies VALUES ${JSON.stringify(getRawBookCopy(copy))}`);
+    }
+
+    public getAllCopies(): Promise<BookCopy[]> {
+        return this.db.any('SELECT * FROM copies')
+            .then(copies => copies.map(copy => this.parseBookCopy(copy)));
     }
 
     // get user with given username
     // PRECONDITION: name is a sanitised string (no special symbols)
     // POSTCONDITION: if user with that username exists, then return user object, otherwise throw error
-    public getUser(name: string) {
+    public getUser(name: string): Promise<User> {
         return this.db.one(`SELECT * FROM users WHERE username='${name}'`);
     }
 
-    public getBooksBorrowedBy(uid: number) {
+    public getUserById(id: number): Promise<User> {
+        return this.db.one(`SELECT * FROM users WHERE id='${id}'`);
+    }
+
+    public getAllBooks(): Promise<Book[]> {
+        return this.db.any(`SELECT * FROM books ORDER BY title, author`);
+    }
+
+    private getBookById(bookId: number): Promise<Book> {
+        return this.db.one(`SELECT * FROM books where id=${bookId}`);
+    }
+
+    // get all books with the given title (or matching titles)
+    // PRECONDITION: title is a sanitised string, but it may contain joker characters (e.g. *)
+    public searchBookByTitle(title: string): Promise<Book[]> {
+        return this.db.any(`SELECT * FROM books WHERE title LIKE "${title}"`);
+    }
+
+    // get all books from the given author (or matching authors)
+    // PRECONDITION: author is a sanitised string, but it may contain joker characters (e.g. *)
+    public searchBookByAuthor(title: string): Promise<Book[]> {
+        return this.db.any(`SELECT * FROM books WHERE title LIKE "${title}"`);
+    }
+
+    public getBooksBorrowedBy(uid: number): Promise<BookCopy[]> {
         return this.db.any(
-        `SELECT books.id AS "bookId", 
-            books.title AS "title", 
-            books.author AS "author", 
-            books.ISBN AS "ISBN",
-            copies.duedate AS "dueDate" 
-        FROM copies INNER JOIN  books ON books.id = copies.book`)
+            `SELECT books.id AS "bookId", 
+                books.title AS "title", 
+                books.author AS "author", 
+                books.ISBN AS "ISBN",
+                copies.duedate AS "dueDate" 
+            FROM copies INNER JOIN  books ON books.id = copies.book`
+        ).then(copies => this.parseBookCopiesByUser(copies, uid));
+    }
+
+    public getCopiesOfBook(bookId: number): Promise<BookCopy[]> {
+        return this.db.any(`SELECT * FROM copies WHERE book = ${bookId}`)
+            .then(copies => this.parseBookCopiesByBook(copies, bookId));
+    }
+
+    private parseBookCopiesByUser(copies: RawBookCopy[], uid: number): Promise<BookCopy[]> {
+        return this.getUserById(uid)
+            .then(user => copies.map(copy => this.parseBookCopy(copy, null, user)))
+            .then(promiseList => Promise.all(promiseList));
+    }
+
+    private parseBookCopiesByBook(copies: RawBookCopy[], bookId: number): Promise<BookCopy[]> {
+        return this.getBookById(bookId)
+            .then(book => copies.map(copy => this.parseBookCopy(copy, book, null)))
+            .then(promiseList => Promise.all(promiseList));
+    }
+
+    private parseBookCopy(copy: RawBookCopy, book: Book = null, user: User = null): Promise<BookCopy> {
+        //find book object
+        let bookObj: Promise<Book> = Promise.resolve(book);
+        if (book === undefined) {
+            bookObj = Promise.resolve<Book>(this.getBookById(copy.book));
+        }
+
+        //find user object
+        let userObj: Promise<User> = Promise.resolve(user);
+        if ((user === undefined) && (copy.borrowerid != null)) {
+            userObj = Promise.resolve(this.getUserById(copy.borrowerid));
+        }
+
+        return Promise.all([bookObj, userObj])
+            .then(bookAndUser => {
+                const book = bookAndUser[0];
+                const user = bookAndUser[1];
+
+                return getBookCopy(user, book, copy);
+            });
     }
 }
--- api/DataTypes.ts
+++ api/DataTypes.ts
@@ -1,6 +1,7 @@
 //TODO cross-check column names and types with the database
 
 import {Moment} from "moment";
+import moment = require("moment");
 
 export interface Book {
     id: number,
@@ -32,4 +33,44 @@ export interface BookCopy {
         borrowDate: Moment,
         dueDate: Moment
     } | null
+}
+
+function getBorrow(user, copy: RawBookCopy) {
+    if (user === null) return null;
+
+    return {
+        borrower: user,
+        borrowDate: moment(copy.borrowdate),
+        dueDate: moment(copy.duedate)
+    };
+}
+
+export function getBookCopy(user: User, book: Book, rawCopy: RawBookCopy): BookCopy {
+    const borrow = getBorrow(user, rawCopy);
+
+    return {
+        barcode: rawCopy.barcode,
+        book: book,
+        borrow: borrow
+    };
+}
+
+export function getRawBookCopy(copy: BookCopy): RawBookCopy {
+    if (copy.borrow == null) {
+        return {
+            barcode: copy.barcode,
+            book: copy.book.id,
+            borrowerid: null,
+            borrowdate:  null,
+            duedate: null
+        };
+    }
+
+    return {
+        barcode: copy.barcode,
+        book: copy.book.id,
+        borrowerid: copy.borrow.borrower.id,
+        borrowdate:  copy.borrow.borrowDate.toISOString(),
+        duedate: copy.borrow.dueDate.toISOString()
+    };
 }
