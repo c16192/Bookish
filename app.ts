88c046b4799610da016fa1cae0508ffadd596287
c16192 <ishidashu@gmail.com>
2018-06-28 16:24:36 +0100

    restructuring api routes

--- app.ts
+++ app.ts
@@ -5,12 +5,12 @@ require('dotenv').config();
 
 import {Request, Response} from "express"
 import BookAPI from "./api/book-api";
+import {initialiseBookAPIRoutes} from "./routes/api-routes";
 
 const cookieSession = require('cookie-session');
 const express = require('express');
 const app = express();
 const dbconnect = new DBConnection();
-const bookAPI = new BookAPI(dbconnect);
 const bodyParser = require("body-parser");
 const Auth = initialiseAuthenticationRoutes(dbconnect);
 const cookieParser = require('cookie-parser');
@@ -23,16 +23,15 @@ app.use(Auth.auth.passport.initialize());
 app.use(Auth.auth.passport.session());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cookieParser());
+
 app.use(Auth.router);
+app.use(initialiseBookAPIRoutes(dbconnect));
 
 app.get('/', (req: Request, res: Response) => {
     res.sendFile(__dirname+'/views/index.html');
 });
 
-app.get('/allBooks', bookAPI.getAllBooks);
-app.get('/getUser', bookAPI.getUserByUserName);
-app.get('/getBooksBorrowed', bookAPI.getBooksBorrowedByUser);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
---
+++ routes/api-routes.ts
@@ -0,0 +1,14 @@
+import Authenticate from "../auth/auth";
+import BookAPI from "../api/book-api";
+import {DBConnection} from "../api/DBConnection";
+
+const routes = require('express').Router();
+
+export function initialiseBookAPIRoutes(dbConnection: DBConnection) {
+    const bookAPI = new BookAPI(dbConnection);
+    routes.get('/allBooks', bookAPI.getAllBooks);
+    routes.get('/getUser', bookAPI.getUserByUserName);
+    routes.get('/getBooksBorrowed', bookAPI.getBooksBorrowedByUser);
+
+    return routes
+}
