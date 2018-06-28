27419877e4f0184f13752546ddfb0e3a072e658b
Marton Erdos <Marton.Erdos@softwire.com>
2018-06-28 11:26:35 +0100

    added user lookup from DB for token generation

--- auth/auth.ts
+++ auth/auth.ts
@@ -1,19 +1,20 @@
+import {DBConnection} from "../api/DBConnection";
+
 const jwt = require('jsonwebtoken');
 const passport = require("passport");
 const passportJWT = require("passport-jwt");
 const ExtractJwt = passportJWT.ExtractJwt;
 const JwtStrategy = passportJWT.Strategy;
-const users = require('../auth/user')
 
 let jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeader(),
     secretOrKey: 'secret'
-}
+};
 
 const strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
     console.log('payload received', jwt_payload);
-    // usually this would be a database call:
-    const user = users.findIndex(users, {id: jwt_payload.id});
+    //TODO: restructure this
+    const user = new DBConnection().getUser(jwt_payload.id);
     if (user) {
         next(null, user);
     } else {
