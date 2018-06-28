a62dd3ef7bb02e35b64201e9f2557de834a35da8
c16192 <ishidashu@gmail.com>
2018-06-28 12:04:45 +0100

    fixed jwtauthheader

--- auth/auth.ts
+++ auth/auth.ts
@@ -7,7 +7,7 @@ const ExtractJwt = passportJWT.ExtractJwt;
 const JwtStrategy = passportJWT.Strategy;
 
 let jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
     secretOrKey: 'secret'
 };
 
